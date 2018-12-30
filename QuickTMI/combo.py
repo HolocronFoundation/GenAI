fcf4a636b4719e655f036fd764dea9cce06e67e9
Samuel Troper <st0567@gmail.com>
2018-12-29 20:23:46 -0500

    Cleaned up

--- QuickTMI/combo.py
+++ QuickTMI/combo.py
@@ -6,32 +6,35 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tqdm import tqdm
+from copy import copy
 
 #from tensorflow.keras.models import Model, Sequential
 #from tensorflow.keras.optimizers import Adam
 #from tensorflow.keras import initializers
 
-def buildGANModel(inputShape, generatorLayers=4, discriminatorLayers=4, defaultLayerSize=16, randomSize=100):
-    generator = buildDNNModel(randomSize, inputShape, generatorLayers, defaultLayerSize)
-    discriminator = buildDNNModel(inputShape, 1, discriminatorLayers, defaultLayerSize) #Change hardcoded output?
-    return [generator, discriminator, GANCompiler([randomSize], generator, discriminator)]
+def buildGANModel(inputShape, generatorLayers=4, discriminatorLayers=4, defaultLayerSize=16, outputs=1, randomSize=100, finalActivation='sigmoid',
+                  optimizers=[keras.optimizers.Adam(lr=0.0002, beta_1=0.5),keras.optimizers.Adam(lr=0.0002, beta_1=0.5), keras.optimizers.Adam(lr=0.0002, beta_1=0.5)]):
+    
+    generator = buildDNNModel(randomSize, inputShape, generatorLayers, defaultLayerSize, optimizer=optimizers[0])
+    discriminator = buildDNNModel(inputShape, outputs, discriminatorLayers, defaultLayerSize, finalActivation=finalActivation, optimizer=optimizers[1])
+    return [generator, discriminator, GANCompiler(randomSize, generator, discriminator, optimizer=optimizers[2]), randomSize]
 
-def GANCompiler(inputSize, generator, discriminator, optimizer=tf.train.AdadeltaOptimizer()):
+def GANCompiler(inputSize, generator, discriminator, optimizer=keras.optimizers.Adam(lr=0.0002, beta_1=0.5), loss='binary_crossentropy'):
     discriminator.trainable = False
     GAN_input = layers.Input(shape=(inputSize,))
     GAN_output = discriminator(generator(GAN_input))
     GAN = keras.Model(inputs=GAN_input, outputs=GAN_output)
-    GAN.compile(loss='binary_crossentropy', optimizer=optimizer)
+    GAN.compile(loss=loss, optimizer=optimizer)
     return GAN
 
 def MNIST_GAN_Test():
     models = buildGANModel(784, [256, 512, 1024], [1024, 512, 256])
-    trainGAN(models, loadMNISTTemp(), 100)
+    trainGAN(models, loadMNISTData(), epochs=100)
 
-def loadMNISTTemp():
+def loadMNISTData():
     (xTrain, yTrain), (x_test, y_test) = keras.datasets.mnist.load_data()
     xTrain = (xTrain.astype(np.float32) - 127.5)/127.5
-    xTrain = xTrain.reshape(60000, 784)
+    xTrain = xTrain.reshape(60000, 784) #Learn more about this reshape
     return [xTrain, yTrain]
 
 def trainGAN(GAN, trainingData, epochs=1, batchSize=128):
@@ -43,7 +46,8 @@ def trainGAN(GAN, trainingData, epochs=1, batchSize=128):
     for epoch in range(epochs):
         print("Epoch: " + str(epoch))
         for i in tqdm(range(batchCount)):
-            noise = np.random.normal(0, 1, size=[batchSize, 100]) #REMOVE HARDCODED 100
+            print()
+            noise = np.random.normal(0, 1, size=[batchSize, GAN[3]])
             batch = xTrain[np.random.randint(0, xTrain.shape[0], size=batchSize)]
 
             generated = GAN[0].predict(noise)
@@ -55,18 +59,17 @@ def trainGAN(GAN, trainingData, epochs=1, batchSize=128):
             GAN[1].trainable = True
             GAN[1].train_on_batch(X, yDis)
 
-            noise = np.random.normal(0, 1, size=[batchSize, 100]) #REMOVE HARDCODED 100
+            noise = np.random.normal(0, 1, size=[batchSize, GAN[3]])
             yGen = np.ones(batchSize)
             GAN[1].trainable = False
-
             GAN[2].train_on_batch(noise, yGen)
         
-        plotGeneratedImages(epoch, GAN[0])
+        plotGeneratedImages(epoch, GAN[0], (28, 28))
 
-def plotGeneratedImages(epoch, generator, examples=100, dim=(10, 10), figsize=(10, 10)):
-    noise = np.random.normal(0, 1, size=[examples, 100]) #REMOVE HARDCODED 100
+def plotGeneratedImages(epoch, generator, imageDimension, examples=100, dim=(10, 10), figsize=(10, 10)):
+    noise = np.random.normal(0, 1, size=[examples, dim[0]*dim[1]])
     generated_images = generator.predict(noise)
-    generated_images = generated_images.reshape(examples, 28, 28)
+    generated_images = generated_images.reshape(examples, imageDimension[0], imageDimension[1])
     plt.figure(figsize=figsize)
     for i in range(generated_images.shape[0]):
         plt.subplot(dim[0], dim[1], i+1)
@@ -75,7 +78,7 @@ def plotGeneratedImages(epoch, generator, examples=100, dim=(10, 10), figsize=(1
     plt.tight_layout()
     plt.savefig('gan_generated_image_epoch_%d.png' % epoch)
 
-def buildDNNModel(inputSize, outputSize, hiddenLayers=4, defaultLayerSize=16, finalActivation='tanh', optimizer=tf.train.AdadeltaOptimizer()):
+def buildDNNModel(inputSize, outputSize, hiddenLayers=4, defaultLayerSize=16, finalActivation='tanh', optimizer=keras.optimizers.Adam(lr=0.0002, beta_1=0.5)):
     # layers is either an integer, or an array of nodes per layer
 
     #Checks that hiddenLayers is a proper input
@@ -93,7 +96,6 @@ def buildDNNModel(inputSize, outputSize, hiddenLayers=4, defaultLayerSize=16, fi
         else:
             model.add(layers.Dense(hiddenLayers[i]))
         model.add(layers.LeakyReLU())
-        #model.add(layers.BatchNormalization())
         model.add(layers.Dropout(.1))
     model.add(layers.Dense(outputSize, activation=finalActivation))
 
@@ -102,53 +104,3 @@ def buildDNNModel(inputSize, outputSize, hiddenLayers=4, defaultLayerSize=16, fi
     return model
 
     #~~~ Current unmarked things - if hiddenlayer is a list, it must be integers.
-
-####
-
-# The dimension of our random noise vector.
-random_dim = 100
-
-def train(epochs=1, batch_size=128):
-    # Get the training and testing data
-    x_train = loadMNISTTemp()[0]
-    y_train = loadMNISTTemp()[1]
-    # Split the training data into batches of size 128
-    batch_count = int(x_train.shape[0] / batch_size)
-
-    # Build our GAN netowrk
-    adam = keras.optimizers.Adam(lr=0.0002, beta_1=0.5)
-    generator = buildDNNModel(random_dim, 784, [256, 512, 1024], optimizer=adam)
-    discriminator = buildDNNModel(784, 1, [1024, 512, 256], finalActivation='sigmoid', optimizer=adam)
-    #gan = get_gan_network(discriminator, random_dim, generator, adam) CHANGED
-    gan = GANCompiler(random_dim, generator, discriminator, adam)
-
-    for e in range(1, epochs+1):
-        print('-'*15, 'Epoch %d' % e, '-'*15)
-        for _ in tqdm(range(batch_count)):
-            # Get a random set of input noise and images
-            noise = np.random.normal(0, 1, size=[batch_size, random_dim])
-            image_batch = x_train[np.random.randint(0, x_train.shape[0], size=batch_size)]
-
-            # Generate fake MNIST images
-            generated_images = generator.predict(noise)
-            X = np.concatenate([image_batch, generated_images])
-
-            # Labels for generated and real data
-            y_dis = np.zeros(2*batch_size)
-            # One-sided label smoothing
-            y_dis[:batch_size] = 0.9
-
-            # Train discriminator
-            discriminator.trainable = True
-            discriminator.train_on_batch(X, y_dis)
-
-            # Train generator
-            noise = np.random.normal(0, 1, size=[batch_size, random_dim])
-            y_gen = np.ones(batch_size)
-            discriminator.trainable = False
-            gan.train_on_batch(noise, y_gen)
-
-        plotGeneratedImages(e, generator)
-
-if __name__ == '__main__':
-    train(400, 128)
